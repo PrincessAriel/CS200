77afe4674634693bbd11f7fd59bc6e5cbb3c47bc
PrincessAriel <esweden@stumail.jccc.edu>
2017-07-12 18:30:42 -0500

    Add files via upload

---
+++ Week 2/Lab 2b - Functions/lab02bfunctionsprogram3.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+float GetDistance(float x1, float y1, float x2, float y2)
+{
+	float distance = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
+	return distance;
+}
+
+int main()
+{
+	while (true)
+	{
+		float x1, y1, x2, y2;
+
+		cout << "First coordinate pair, enter x and y:  ";
+		cin >> x1 >> y1;
+
+		cout << "Second coordinate pair, enter x and y:  ";
+		cin >> x2 >> y2;
+
+		float distance = GetDistance(x1, y1, x2, y2);
+		cout << "Distance:  " << distance << endl;
+
+		cout << endl << endl;
+	}
+
+	while (true);
+	return 0;
+}
