7a2b0afb628cfa218b9cf6656f878d32fd0255c5
PrincessAriel <esweden@stumail.jccc.edu>
2017-07-13 20:52:28 -0500

    Add files via upload

---
+++ Week 1/Lab 1a - Variables and I-O/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Sugar Cookie Recipe:" << endl << endl; 
+	cout << "1 tsp      baking soda" << endl;
+	cout << "1/2 tsp    baking powder" << endl;
+	cout << "1 c        butter" << endl;
+	cout << "1 1/2 c    white sugar" << endl;
+	cout << "1          egg" << endl;
+	
+	while (true);
+	return 0;
+}
