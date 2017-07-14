ae37116bf1a8618f8860b64122593934bde88aa4
PrincessAriel <esweden@stumail.jccc.edu>
2017-07-13 20:25:53 -0500

    Add files via upload

---
+++ Week 3/Lab 3a - Testing/lab03a_function1.hpp
@@ -0,0 +1,73 @@
+#ifndef _function1
+#define _function1
+
+
+int AddThree( int a, int b, int c )
+{
+	int sum = a + b + c;  /*Revised from former display: return a + b + c.*/
+    return sum;
+}
+
+/* Add a test to this function */
+void Test_AddThree()
+{
+    cout << "************ Test_AddThree ************" << endl;
+
+    int input1, input2, input3;
+    int expectedOutput;
+    int actualOutput;
+
+    /* TEST 1 ********************************************/
+    input1 = 1; input2 = 1; input3 = 1;
+    expectedOutput = 3;
+
+    actualOutput = AddThree( input1, input2, input3 );
+    if ( actualOutput == expectedOutput )
+    {
+        cout << "Test_AddThree: Test 1 passed!" << endl;
+    }
+    else
+    {
+        cout << "Test_AddThree: Test 1 FAILED!" << endl;
+    }
+
+    /* TEST 2 ********************************************/
+    // CREATE YOUR OWN TEST
+    input1 = -9;             // change me
+    input2 = 0;             // change me
+    input3 = 7;             // change me
+    expectedOutput = -2;    // change me
+
+
+    // Run test (keep this as-is):
+    actualOutput = AddThree( input1, input2, input3 );
+    if ( actualOutput == expectedOutput )
+    {
+        cout << "Test_AddThree: Test 2 passed!" << endl;
+    }
+    else
+    {
+        cout << "Test_AddThree: Test 2 FAILED!" << endl;
+    }
+
+    /* TEST 3 ********************************************/
+    // CREATE YOUR OWN TEST
+    input1 = -7;             // change me
+    input2 = 0;             // change me
+    input3 = 12;             // change me
+    expectedOutput = 5;    // change me
+
+
+    // Run test (keep this as-is):
+    actualOutput = AddThree( input1, input2, input3 );
+    if ( actualOutput == expectedOutput )
+    {
+        cout << "Test_AddThree: Test 3 passed!" << endl;
+    }
+    else
+    {
+        cout << "Test_AddThree: Test 3 FAILED!" << endl;
+    }
+}
+
+#endif
